1103c2397bffae1d18e75170481fe51ab0c62ee5
Damian <AdrianAndradeChristian@outlook.com>
2023-02-14 21:44:04 -0500

    Adding controllers, views and models

---
+++ MovieApp/MvcMovie/Data/MvcMovieContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace MvcMovie.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; } = default!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (OracleConfiguration.TnsAdmin is null)
+            {
+                OracleConfiguration.TnsAdmin = @"C:\Users\Fmla\Documents\OracleWallet\MyERP\";
+                OracleConfiguration.WalletLocation = OracleConfiguration.TnsAdmin;
+            }
+        }
+    }
+}
